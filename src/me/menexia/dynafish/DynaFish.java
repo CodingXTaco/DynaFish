b07aead3fa471f3693f15ee0023c90b031cf099e
Xavi Ablaza <xlablaza@gmail.com>
2011-11-26 21:49:51 +0800

    Fixed onCommand, cleaned code

--- src/me/menexia/dynafish/DynaFish.java
+++ src/me/menexia/dynafish/DynaFish.java
@@ -24,7 +24,6 @@ public class DynaFish extends JavaPlugin {
 	private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
 	//Create HashMap Debugee
 
-
 @Override
 public void onDisable() {
 	this.logger.info("DynaFish disabled!");
@@ -39,20 +38,15 @@ this.logger.info( pdf.getName() + " version " + pdf.getVersion() + " is enabled!
 }
 // End of onEnable method.
 
-
-
-
-
 public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		if (commandLabel.equalsIgnoreCase("dynafish")
 			|| commandLabel.equalsIgnoreCase("df"))
 		{
 		toggleVision((Player) sender);
 		}
-	return false;
+	return true;
 		}
 // End of onCommand method.
-
 
 public boolean isDebugging(final Player player) {
 	if (debugees.containsKey(player)) {
@@ -60,7 +54,6 @@ public boolean isDebugging(final Player player) {
 	} else {
 		return false;
 	}
-	
 }
 // End of debugging method.
 
@@ -68,15 +61,11 @@ public void setDebuging(final Player player, final boolean value) {
 	debugees.put(player, value);
 }
 // The method enabled which checks to see if the player is in the hashmap.
-
 public boolean enabled(Player player) {
 	return this.DFUsers.containsKey(player);
-		// Check if has player within key.
 	}
-
-
-
-
+	// Check if it has player within key.
+	
 	public void toggleVision(Player player) {
 		if (enabled(player)) {
 			this.DFUsers.remove(player);
@@ -86,11 +75,5 @@ public boolean enabled(Player player) {
 			player.sendMessage("DynaFish enabled!");
 		}
 	}
-	// End of toggleVision method
-	
-	
-	
-	
-	
-	
+	// End of toggleVision method	
 }
