688bf7411a77b082f6ad441f3af92c33ddfba4ea
Xavi Ablaza <xlablaza@gmail.com>
2011-11-27 03:30:38 +0800

    experimental (multi-stack?) commit, unclean

--- src/me/menexia/dynafish/DFEntityListener.java
+++ src/me/menexia/dynafish/DFEntityListener.java
@@ -1,9 +1,11 @@
 package me.menexia.dynafish;
 
+import java.util.List;
 import java.util.Random;
 
+import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.block.BlockFace;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.entity.EntityExplodeEvent;
@@ -19,6 +21,30 @@ public class DFEntityListener extends EntityListener {
 	
 	public void onEntityExplode(EntityExplodeEvent event) {
 Entity ent = event.getEntity();
+if (ent instanceof TNTPrimed && (ent.getLocation().getBlock().getRelative(org.bukkit.block.BlockFace.UP).isLiquid())) {
+int ic = 9;
+List<Block> blocks = event.blockList();
+Block block = blocks.get(ic);
+Location pos = block.getLocation();
+Random random = new Random();
+int amt = random.nextInt(32);
+ItemStack i = new ItemStack(349);
+i.setAmount(amt);
+block.getWorld().dropItemNaturally(pos, i);
+
+
+}
+
+
+	}
+	// End of EntityExplodeEvent method.
+	
+	
+	
+	
+	
+	/*public void onEntityExplode(EntityExplodeEvent event) {
+Entity ent = event.getEntity();
 if (ent instanceof TNTPrimed && (ent.getLocation().getBlock().getRelative(BlockFace.UP).isLiquid())) {
 	World w = event.getLocation().getWorld();
 	Random random = new Random();
@@ -27,6 +53,6 @@ if (ent instanceof TNTPrimed && (ent.getLocation().getBlock().getRelative(BlockF
 	i.setAmount(amt);
 	w.dropItemNaturally(event.getEntity().getLocation(), i);
 }
-	}
-	// End of the onEntityExplode method.
+	}*/
+	// End of the OLD onEntityExplode method.
 }
