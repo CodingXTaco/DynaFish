4c22bb8c3e3fb001a7ac70e9a2603c83ad9604cf
Xavi Ablaza <xlablaza@gmail.com>
2011-11-29 19:46:51 +0800

    found a way to spread drops! need to clean code.

--- src/me/menexia/dynafish/DFEntityListener.java
+++ src/me/menexia/dynafish/DFEntityListener.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.TNTPrimed;
@@ -26,19 +27,29 @@ public class DFEntityListener extends EntityListener {
 		if (event.isCancelled()) return;
 Entity ent = event.getEntity();
 if (ent instanceof TNTPrimed && (ent.getLocation().getBlock().getRelative(org.bukkit.block.BlockFace.UP).isLiquid())) {
+	Random value = new Random();
+	int chance = value.nextInt(10);
+	if (chance < 1) {} else {
 Location loc = ent.getLocation();
 World w = loc.getWorld();
 /*event.setCancelled(true);
 w.createExplosion(loc, 0.0F, false);
 w.playEffect(loc, Effect.SMOKE, 1);*/
+//(random.nextInt(5) - 2, 0, random.nextInt(5) - 2)
+//getRelative(random.nextInt(5) - 2, 0, random.nextInt(5)- 2);
 Random random = new Random();
-int x = ent.getLocation().getBlockX();
+int x = ent.getLocation().getBlock().getRelative(random.nextInt(6) - 3, 0, random.nextInt(6)- 3).getX();
 int y = ent.getLocation().getBlockY();
-int z = ent.getLocation().getBlockZ();
-int amt = random.nextInt(32);
+int z = ent.getLocation().getBlock().getRelative(random.nextInt(6) - 3, 0, random.nextInt(6)- 3).getZ();
+int amt = random.nextInt(2);
 ItemStack i = new ItemStack(349);
 i.setAmount(amt);
-w.dropItemNaturally(new Location(w, x, y, z), i);
+for (int i1=0; i1<16; i1++) {
+	w.dropItemNaturally(new Location(w, x, y++, z), i);
+}
+for (int a1=0; a1<16; a1++) {
+	w.dropItemNaturally(new Location(w, z, y++, x), i);
+}
 //(random.nextInt(5) - 2, 0, random.nextInt(5) - 2);
 
 	/*double x = ent.getLocation().getX();
@@ -46,6 +57,8 @@ w.dropItemNaturally(new Location(w, x, y, z), i);
 	double z = ent.getLocation().getZ();*/
 	
 }
+}
+// end of else statement
 	}
 	// NEW EXPLODE METHOD.
 	
