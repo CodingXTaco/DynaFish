b23777f23b9b19eca24757d4a3a23ef2152b929e
Xavi Ablaza <xlablaza@gmail.com>
2011-12-05 10:00:51 +0800

    need to change values into percentages for v1.4

--- src/me/menexia/dynafish/DFEntityListener.java
+++ src/me/menexia/dynafish/DFEntityListener.java
@@ -12,6 +12,7 @@ import org.bukkit.inventory.ItemStack;
 
 public class DFEntityListener extends EntityListener {
 	public static DynaFish plugin;
+	private static Random random = new Random();
 	public DFEntityListener(DynaFish instance) {
 		plugin = instance;
 	}
@@ -22,19 +23,25 @@ public class DFEntityListener extends EntityListener {
 				if (plugin.user.contains(pl)) {
 Entity ent = event.getEntity();
 if (ent instanceof org.bukkit.entity.TNTPrimed && (ent.getLocation().getBlock().getRelative(org.bukkit.block.BlockFace.UP).isLiquid())) {
-	Random value = new Random();
-	int chance = value.nextInt(10);
-	if (chance < 1) {} else {
-Location sym = ent.getLocation();
-World shu = sym.getWorld();
-Random random = new Random();
-double x = sym.getX() + random.nextInt(5) - 2;
-double y = sym.getY() + 1;
-double z = sym.getZ() + random.nextInt(5) - 2;
-for (int a=0; a<33; a++) {
-	int p = random.nextInt(2);
-	if (p < 1) continue;
-	shu.dropItemNaturally(new Location(shu, x, y, z), new ItemStack(349, 1));
+	if (DynaFish.OVERALL_CHANCE > 0) {
+		Location sym = ent.getLocation();
+		World inull = sym.getWorld();
+		final int ersh = random.nextInt(DynaFish.OVERALL_CHANCE) + 1;
+		if (ersh == 1) {
+			double x = sym.getX() + random.nextInt(5) - 2;
+			double y = sym.getY() + 1;
+			double z = sym.getZ() + random.nextInt(5) - 2;
+			for (int a=0; a<33; a++) {
+				int p = random.nextInt(DynaFish.CHANCE_PER_DROP) + 1;
+				// bah, math. chance per drop needs to be somewhat incremental.
+				// if user sets chance per drop as 2...
+				// TODO: NEED TO MAKE INTO PERCENTAGES.
+				if (p == 1) continue;
+				inull.dropItemNaturally(new Location(inull, x, y, z), new ItemStack(349, 1));
+		}
+
+	} else {
+
 } // end of for loop
 } // end of else statement
 } // end of if TNTPrimed
